3ee5d3a55b4a1f30a6da1de3bee5be61ea5e0a2b
zhihaozhang <zhihaozhang@me.com>
2016-03-20 16:42:07 +0800

    update pearson function

--- public/pearson.js
+++ public/pearson.js
@@ -1,37 +1,15 @@
-function get_pearson_co(X, Y){
- 
-    if (X.length != Y.length){
-        console.log('wrong length');
+function calculate(x,y){
+    var sumx = 0,sumy = 0,sumSqrx = 0,sumSqry = 0,sumXY = 0;
+    for(var i=0;i< x.length;++i){
+        sumx += x[i];
+        sumy += y[i];
+
+        sumSqrx += x[i]*x[i];
+        sumSqry += y[i]*y[i];
+
+        sumXY += x[i]*y[i];
     }
-    
-    
-    var n = X.length;
-    //console.log(n);
-    
-    var aveX ,aveY;
-    aveX = 0; aveY = 0;
-    var p;
-    for (var i = 0; i < n; i++)aveX += X[i];
-    aveX = (aveX * 1.0) / n;
-    for (var i = 0; i < n; i++)aveY += Y[i];
-    aveY = (aveY * 1.0) / n;
-    
-    
-    //console.log(aveX, aveY);
-    
-    var t0, t1, t2;
-    t0 = 0; t1 = 0; t2= 0; 
-    for (var i = 0; i < n; i++){
-        t0 += (X[i] - aveX) * (Y[i] - aveY);
-        t1 += (X[i] - aveX) * (X[i] - aveX);
-        t2 += (Y[i] - aveY) * (X[i] - aveY); 
-    }
-    
-   // console.log(t0, t1, t2);
-    
-    var result;
-    result = t0 * 1.0 / Math.sqrt(t1 * t2);
-    
-//    console.log(result);
-    return result;
+    var ret = (sumXY* x.length - sumx * sumy) / (Math.sqrt((sumSqrx* x.length - sumx*sumx)) * Math.sqrt((sumSqry* y.length - sumy*sumy)) );
+    console.log(ret);
+    return ret;
 }
